e9baf6116396be9f82433d0456c32d9578b30a20
Ingenieria Sistemas11 <isi11@labs.uca.edu.ni>
2019-07-08 15:52:10 -0600

    first commit

---
+++ ServicioWeb/Producto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicioWeb
+{
+    public class Producto
+    {
+        public Producto(int idProducto, string description, string detalle, int stock, decimal precio, string imagen)
+        {
+            this.idProducto = idProducto;
+            this.description = description;
+            this.detalle = detalle;
+            this.stock = stock;
+            this.precio = precio;
+            this.imagen = imagen;
+        }
+
+        public Producto()
+        {
+            this.idProducto = 0;
+            this.description = "";
+            this.detalle = "";
+            this.stock = 0;
+            this.precio = 0;
+            this.imagen = "";
+
+        }
+
+        public int idProducto { get; set; }
+        public string description { get; set; }
+        public string detalle { get; set; }
+        public int stock { get; set; }
+
+        public decimal precio { get; set; }
+        public string imagen { get; set; }
+        
+
+      }
+    }
+}
